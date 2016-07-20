241c35f32f078a782caf75cb7ee5c186b3d3f975
VulcanoAhab <vulcano.ahab@gmail.com>
2016-07-20 20:38:34 -0300

    Simple server shell snippets

--- simple_ssh_http/serverBasicWall.sh
+++
@@ -1,9 +0,0 @@
-
-#install fail2bain
-echo "[•] Starting fail2bain instalation"
-sudo apt-get install fail2ban
-
-# -------- set up firewall
-sudo iptables-restore ipv4_only_ssh
-sudo ip6tables-restore ipv6_close
-sudo service iptables restart
